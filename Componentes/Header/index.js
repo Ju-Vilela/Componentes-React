2d3acaa5ba7c1d6e966909e0d85f3f1cedb3b2c8
Julya <86087548+Ju-Vilela@users.noreply.github.com>
2022-07-13 20:11:02 -0300

    Componentes da página
    Conteudos que vão ser exportados para o principal.

---
+++ Componentes/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css'
+
+function Header() {
+    return (
+        
+        <header>Header Component</header>
+    
+    );
+}
+
+export default Header;
---
+++ Componentes/HomePag/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from '../Card/';
+import './homepag.css';
+
+export default function HomePag() {
+    return (
+
+        <div className="home-page">
+
+            <Card />
+            <Card />
+            <Card />
+
+        </div>
+
+    );
+}
